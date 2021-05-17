c404a1a6288bd9b6225d7c1e92b2483e5827a7fc
Carlos Vargas <carlosvargas@hotmail.com>
2021-05-17 12:17:40 -0400

    Create demoenv.sh

---
+++ demoenv.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Script to build Datto SE Environment
+
+#####################
+# Variables
+####################
+#$logfilename=demobuildout-`date +%m%d%y%I%M%S`.log
+
+print "Starting of Datto SE Build Out Script... "
+
+clear
+echo "Welcome to the Datto SE Azure Build Environment Script"
+echo ""
+echo "What's your username? Example: cvargas"
+read demouser
+
+echo "Type passwords for your vm's. 12 Characters minimum Example: P@ssw0rd1234"
+read -s vmpass
+
+#Test Variables
+#echo $demouser
+#echo $vmpass
+
+# Create Resource Group for Demo Environment
+echo "Creating Demo Resource Group... "
+az group create -l eastus -n $demouser-DEMO-RG --tags "owner"="$demouser"
+
+# Create Network for Demo Environment
+echo "Creating Demo Network..."
+az network vnet create -g $demouser-DEMO-RG -n DemoNetwork --address-prefix 10.0.0.0/16 --subnet-name DemoSubnet --subnet-prefix 10.0.0.0/24
+
+# Create VM for Demo Environment
+echo "Creating VM for Demo... - Please wait"
+az vm create -n demovm1 -g $demouser-DEMO-RG --image Win2016Datacenter --size Standard_B1s --admin-username $demouser --admin-password $vmpass
+
+# Create Restore Resource Group
+echo "Creating Resource Group for Restore Test..."
+az group create -l eastus -n datto-restore --tags "owner"="$demouser"
+
+#Crete Restore Network
+echo "Creating Network Resources for Restore Test..."
+ az network vnet create -g datto-restore -n datto-restore-vnet-eastus --address-prefix 10.0.0.0/16 --subnet-name default --subnet-prefix 10.0.0.0/24
+
+#Finish Script
